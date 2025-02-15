9480ea3f47f92aea6425ae5462290321113f7d75
newtsarthur <tutuhoficial13@gmail.com>
2025-02-15 13:03:38 -0300

    criação das variáveis nome e sobrenome

--- conta-banco2/src/ContaTerminal.java
+++ conta-banco2/src/ContaTerminal.java
@@ -5,13 +5,21 @@ public class ContaTerminal {
     //TODO: Conhecer e importar a classe Scanner
     Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Seja bem vindo ao nosso aplicativo!");
+
+    System.out.print("Olá! Por favor, digite seu primeiro nome: ");
+    String firstName = scanner.nextLine();
+
+    System.out.print("Agora digite seu sobrenome: ");
+    String lastName = scanner.nextLine();
+
     System.out.print("Por gentileza, digite o número da Agência: ");
     String agencia = scanner.nextLine();
 
     System.out.print("Agora, digite os números da sua conta: ");
     int numero = scanner.nextInt();
 
-    
+
     //Exibir pelo scanner os valores digitados no terminal
 
     //Exibir a mensagem criada
