318f36875d1124e4f38e28d6aec567e0c9e842df
newtsarthur <tutuhoficial13@gmail.com>
2025-02-15 12:53:26 -0300

    Adicionar o número da agência através do scanner

--- conta-banco2/src/ContaTerminal.java
+++ conta-banco2/src/ContaTerminal.java
@@ -5,7 +5,8 @@ public class ContaTerminal {
     //TODO: Conhecer e importar a classe Scanner
     Scanner scanner = new Scanner(System.in);
 
-    
+    System.out.print("Por gentileza, digite o número da Agência: ");
+    int numero = scanner.nextInt();
 
     //Exibir pelo scanner os valores digitados no terminal
 
