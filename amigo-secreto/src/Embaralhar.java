5b9d801d59f724d4a198b79f8fc04410625beb48
newtsarthur <tutuhoficial13@gmail.com>
2025-02-22 02:47:46 -0300

    adicionar nomes ao arrayList

--- amigo-secreto/src/Embaralhar.java
+++ amigo-secreto/src/Embaralhar.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 public class Embaralhar {
   public static void main(String[] args) {
     Scanner ler = new Scanner(System.in);
+    
     System.out.print("Digite o total de amigos que você quer sortear: ");
     int total = ler.nextInt();
+    ler.nextLine();
 
     String[] amigosTotal = new String[total];
 
     try {
-      for(int x = 0; x <= total; x++)
+      for(int x = 0; x < total; x++)
       {
         System.out.print("Digite o nome que você quer adicionar a lista de amigos sorteados: ");
         String amigos = ler.nextLine();
-
         amigosTotal[x] = amigos;
 
-        System.out.println(amigosTotal);
       }
     } catch (Exception e) {
       // TODO: handle exception
