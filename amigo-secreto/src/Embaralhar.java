a3a2f6bc4fd946deb2db691894878b67e65a7df2
newtsarthur <tutuhoficial13@gmail.com>
2025-02-22 02:29:17 -0300

    tentativa de adicionar nomes a lista

--- amigo-secreto/src/Embaralhar.java
+++ amigo-secreto/src/Embaralhar.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 public class Embaralhar {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     Scanner ler = new Scanner(System.in);
-    System.out.println("Digite o total de amigos que você quer sortear: ");
+    System.out.print("Digite o total de amigos que você quer sortear: ");
     int total = ler.nextInt();
 
-    String amigosTotal[];
+    String[] amigosTotal = new String[total];
 
     try {
       for(int x = 0; x <= total; x++)
       {
-        System.out.println("Digite o nome que você quer adicionar a lista de amigos sorteados: ");
+        System.out.print("Digite o nome que você quer adicionar a lista de amigos sorteados: ");
         String amigos = ler.nextLine();
+
+        amigosTotal[x] = amigos;
+
+        System.out.println(amigosTotal);
       }
     } catch (Exception e) {
       // TODO: handle exception
-      System.out.print("Erro ");
+      System.out.print("Erro");
     }
   }
 }
