9abade2fbc455674d6638abe0545e4975a1faab6
newtsarthur <tutuhoficial13@gmail.com>
2025-02-22 20:07:02 -0300

    contador em java

--- controle-fluxo/src/Contador.java
+++ controle-fluxo/src/Contador.java
@@ -1,8 +1,38 @@
 import java.util.Scanner;
 
 public class Contador {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     Scanner terminal = new Scanner(System.in);
-    System.out.println("Digite ")
+
+    System.out.print("Digite o primeiro parâmetro: ");
+    int parametroOne = terminal.nextInt();
+
+    System.out.print("Digite o segundo parâmetro: ");
+    int parametroTwo = terminal.nextInt();
+
+    try {
+      contar(parametroOne, parametroTwo);
+    } catch (ParametrosInvalidosExeption e) {
+      System.out.println("Erro!" + e.getMessage());
+    } finally {
+      terminal.close();
+    }
+  } 
+
+  static void contar(int parametroOne, int parametroTwo) throws ParametrosInvalidosExeption {
+    if(parametroOne >= parametroTwo) {
+      throw new ParametrosInvalidosExeption("O segundo parâmetro deve ser maior que o primeiro parâmetro");
+    }
+    int contagem = parametroTwo - parametroOne;
+
+    for(int i = 1; i <= contagem; i++) {
+      System.out.println("Imprimindo o número " + (parametroOne + i));
+    }
+  }
+}
+class ParametrosInvalidosExeption extends Exception {
+  public ParametrosInvalidosExeption(String mensagem)
+  {
+    super(mensagem);
   }
 }
